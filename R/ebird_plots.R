9181cd74299c5d7471377372f55b227df398e3d8
Raahil Madhok <raahil_madhok@hks.harvard.edu>
2019-01-22 01:09:04 -0800

    added spatial coverage computation

--- R/ebird_plots.R
+++ R/ebird_plots.R
@@ -7,6 +7,7 @@
 ROOT <- '/Users/rmadhok/Documents/ubc/research/def_biodiv/'
 EBIRD.GEO <- paste(ROOT, 'data/csv/', sep='')
 DIST.GEO <- '/Users/rmadhok/Dropbox (CID)/IndiaPowerPlant/data/maps/india-district/'
+STATE.GEO <- '/Users/rmadhok/Dropbox (CID)/IndiaPowerPlant/data/maps/datameet_maps/States/'
 SAVE.PATH <- paste(ROOT, 'docs/tex_doc/fig/', sep='')
 
 # Packages
@@ -15,11 +16,14 @@ require(rgdal)
 require(raster)
 require(ggplot2)
 require(geosphere)
+require(viridis)
 
 ## Read Data
 
-# Read District Map
+# Read Maps
 districts <- readOGR(DIST.GEO,'SDE_DATA_IN_F7DSTRBND_2011',stringsAsFactors = F)
+states <- readOGR(STATE.GEO, 'Admin2', stringsAsFactors = F)
+
 
 # Read eBird
 proportion <- 5
@@ -50,25 +54,54 @@ ggplot() +
         axis.title.y = element_blank(),
         legend.position = 'bottom',
         legend.title=element_text(size=20) , legend.text=element_text(size=10))
-ggsave(paste(SAVE.PATH, 'ebird_', proportion, 'all_2017.pdf', sep=''), width=8,height=6)
+ggsave(paste(SAVE.PATH, 'ebird_', proportion, '_all_2017.pdf', sep=''), width=8,height=6)
 
 #   2. Biodiviersity (2017)
 
 # Index
-biodiversity.df <- def_biodiv[def_biodiv$year==2017, c('c_code_2011', 'year', 'month', 'shannon_index')]
+biodiversity.df <- def_biodiv[def_biodiv$year==2017, c('c_code_2011', 'year', 'month', 'shannon_index', 'district_forest_cum')]
 
 # Aggregate to year
-biodiversity.2017.df <- aggregate(biodiversity.df$shannon_index,
-                                  by=list(c_code_11=biodiversity.df$c_code_2011), 
-                                  mean)
-names(biodiversity.2017.df)[2] <- 'shannon_index'
+biodiversity.2017.df <- aggregate(.~c_code_2011, biodiversity.df, mean)
+names(biodiversity.2017.df)[1] <- 'c_code_11'
 
 # Merge with district attribute table
 districts <- merge(districts, biodiversity.2017.df, by='c_code_11')
 
-# Plot
-ggplot(data = districts, aes(x=long,y=lat, group=group)) +
-  geom_sf(aes(fill='shannon_index')) + 
+# PLot
+p <- ggplot() +
+  # municipality polygons
+  geom_polygon(data = districts, aes(fill = districts@data$shannon_index, 
+                                    x = long, 
+                                    y = lat, 
+                                    group = group)) +
+  # municipality outline
+  geom_path(data = districts, aes(x = long, 
+                                 y = lat, 
+                                 group = group), 
+            color = "white", size = 0.1) +
+  coord_equal()
+
+
+map <- ggplot() +
+  geom_polygon(data=districts, color='grey', size=.5, aes(x=long,y=lat,group=group, fill=districts@data$shannon_index)) +
+  coord_equal() + theme_minimal() + 
+  scale_fill_viridis(option = "magma", direction = -1) +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.text.y = element_blank(),  
+        axis.ticks = element_blank(), 
+        axis.title.x = element_blank(),  
+        axis.title.y = element_blank(),
+        legend.position = 'bottom',
+        legend.title=element_text(size=20) , legend.text=element_text(size=10))
+ggsave(paste(SAVE.PATH, 'biodiversity_2017.pdf', sep=''), width=8,height=6)
+
+map <- ggplot(data=districts, aes(x=long,y=lat,group=group), 
+       color='grey', size=.5) +
+  geom_polygon(aes(fill=districts@data$index)) +
   coord_equal() + theme_minimal() + 
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
@@ -80,7 +113,5 @@ ggplot(data = districts, aes(x=long,y=lat, group=group)) +
         axis.title.y = element_blank(),
         legend.position = 'bottom',
         legend.title=element_text(size=20) , legend.text=element_text(size=10))
-
-ggsave(paste(SAVE.PATH, 'biodiversity_2017.pdf', sep=''), width=8,height=6)
 
 
--- R/process_ebird.R
+++ R/process_ebird.R
@@ -13,13 +13,20 @@ setwd(read_path_head)
 # Load Packages
 require(data.table)
 require(rgdal)
+require(rgeos)
+require(sp)
+require(raster)
 require(splitstackshape)
 
 #### SET PROPORTION TO RANDOMLY SAMPLE
 proportion <- .05
 ####
 
-# Read Data
+# Load india 2011 districts
+india.districts.2011 <- readOGR(paste(dist_shpf, "maps/india-district", sep=""), 
+                                'SDE_DATA_IN_F7DSTRBND_2011', stringsAsFactors = F)
+
+# Read eBird Data
 df <- fread('ebd_IN_200001_201810_relOct-2018.txt')
 
 # Keep 2014-2018
@@ -27,17 +34,12 @@ df$YEAR <- as.numeric(strftime(df[['OBSERVATION DATE']], format = "%Y"))
 df$YEARMONTH <- strftime(df[['OBSERVATION DATE']], format = "%Y-%m")
 df <- df[df$YEAR > 2013, ]
 
-# Load india 2011 districts
-india.districts.2011 <- readOGR(paste(dist_shpf, "maps/india-district", sep=""), 
-                                'SDE_DATA_IN_F7DSTRBND_2011', stringsAsFactors = F)
-
 ### Identify District Census Code
-
-# Overlay eBird on District Polygons
 coords <- df[, 27:26]
 proj <- proj4string(india.districts.2011)
 df$c_code_2011 <- over(SpatialPoints(coords, proj4string=CRS(proj)), india.districts.2011)$c_code_11
-df <- df[!is.na(df$c_code_2011),] 
+df <- df[!is.na(df$c_code_2011),]
+rm(list='coords')
 
 # Sample from each district-year
 set.seed(12345)
@@ -52,3 +54,30 @@ write.csv(df.sample,
           paste(save_path_head, 'csv/ebird_sample_', sample.prop, '.csv', sep=""),
           row.names = F)
 
+### Spatial Coverage
+
+# Initialize Grid
+grid <- raster(extent(india.districts.2011))
+res(grid) <- .1 # grid resolution in degrees lat-lon
+proj4string(grid) <- proj4string(india.districts.2011) #sync coordinate systems of gridded district and original vector data
+
+# Clip to Shapefile Extent
+india.grid.crop <- crop(grid, extent(india.districts.2011))
+india.grid <- mask(india.grid.crop, india.districts.2011)
+
+# Count birds in each cell
+coords <- df.sample[,15:14]
+india.grid <- rasterize(coords, india.grid, fun='count')
+bird.count <- as.data.frame(india.grid, xy=TRUE)
+names(bird.count) <- c('lon', 'lat', 'count')
+
+# Get District Codes
+bird.count$c_code_2011 <- over(SpatialPoints(bird.count[,1:2], proj4string=CRS(proj)), india.districts.2011)$c_code_11
+bird.count <- bird.count[!is.na(bird.count$c_code_2011),]
+bird.count$count[is.na(bird.count$count)] <- 0
+
+# Write
+grid.res <- res(grid)[1]*100
+write.csv(bird.count, 
+          paste(save_path_head, 'csv/count_grid_', grid.res,'km_sample_', sample.prop, '.csv', sep=""),
+          row.names = F)
