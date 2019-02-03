5c48ea42740aa2fdf892dfbc957354d67bc83d82
Raahil Madhok <raahil_madhok@hks.harvard.edu>
2019-02-03 13:27:58 -0800

    added script for processing satellite weather data

---
+++ R/process_weather.R
@@ -0,0 +1,101 @@
+# PROJECT: Deforestation and Biodiversity
+# PURPOSE: Process Weather Data [Source: ERA-Interim]
+# AUTHOR: Raahil Madhok
+# DATE: Feb 2 2019 [created]
+
+### SET-UP
+# Directories
+path_head <- '/Users/rmadhok/Documents/ubc/research/def_biodiv/data/'
+dist_shpf <- '/Users/rmadhok/Dropbox (CID)/IndiaPowerPlant/data/'
+setwd(path_head)
+
+# Packages
+require(ncdf4) 
+require(raster)
+require(rgdal) 
+require(sp)
+require(dplyr)
+
+# Load india 2011 districts
+india.districts.2011 <- readOGR(paste(dist_shpf, "maps/india-district", sep=""), 
+                                'SDE_DATA_IN_F7DSTRBND_2011', stringsAsFactors = F)
+
+### Process Weather
+weather <- list('temperature', 'precipitation')
+
+# Initiate Master dataframes
+df.temperature <- data.frame()
+df.precipitation <- data.frame()
+
+# Populate Dataframes
+for (i in weather) {
+  
+  for (year in 2014:2018) {
+    
+    print(paste('Processing ', i, ': ', year, sep=''))
+    
+    # Read NetCDF
+    stack <- stack(paste(path_head, "netcdf/", i, '_' , year, '.nc', sep=""))
+    
+    # Resolution/Projection
+    res(stack) <- 0.5
+    proj <- proj4string(india.districts.2011)
+    proj4string(stack) <- proj  #sync coordinate systems of grid and original vector data
+    
+    # Crop Borders
+    stack <- crop(stack, extent(india.districts.2011))
+    stack <- mask(stack, india.districts.2011)
+    
+    for (month in 1:12){
+      
+      # Dataframe
+      df.month <- as.data.frame(stack[[month]], xy=TRUE)
+      colnames(df.month)[3] <- 'mean'
+      
+      # Get District Codes
+      proj <- proj4string(india.districts.2011)
+      df.month$c_code_2011 <- over(SpatialPoints(df.month[,1:2], proj4string=CRS(proj)), india.districts.2011)$c_code_11
+      df.month <- df.month[!is.na(df.month$c_code_2011), ]
+
+      # Aggregate
+      df.month <- df.month %>% 
+        group_by(c_code_2011) %>% 
+        summarise(mean = mean(mean))
+      
+      # Timestamp
+      mth <- sprintf("%02d", month)
+      df.month$yearmonth <- paste(year, mth, sep='-')
+      
+      # Append
+      if (i == 'temperature') {
+       
+         df.temperature <- rbind(df.temperature, df.month)
+      
+         }
+      else{
+        
+        df.precipitation <- rbind(df.precipitation, df.month)
+      }
+      
+    }
+    
+  }
+  
+}
+
+# Convert Kelvin to Celsius
+colnames(df.temperature)[2] <- 'temperature_mean'
+df.temperature$temperature_mean <- df.temperature$temperature_mean - 273.15
+
+# Convert rainfall from meter to millimeters
+colnames(df.precipitation)[2] <- 'precipitation_mean'
+df.precipitation$precipitation_mean <- df.precipitation$precipitation_mean * 1000
+
+# Merge
+df.weather <- merge(df.temperature, df.precipitation, by=c('c_code_2011', 'yearmonth'))
+
+# Save
+write.csv(df.weather, 
+          paste(path_head, 'csv/india_weather.csv', sep=""),
+          row.names = F)
+
